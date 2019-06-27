c1dce85cd103042aafd955c4ce6eec252ef62604
Muriel S <muriel-souza@live.com>
2019-06-27 00:34:49 -0300

    adicionando on_delete. Requisito da nova versão do python Django

--- lpc-event/lpc_evento/eventos/models.py
+++ lpc-event/lpc_evento/eventos/models.py
@@ -14,7 +14,7 @@ class Pessoa(models.Model):
     nome = models.CharField(max_length=128)
     descricao = models.TextField()
     data_nascimento = models.DateField(blank=True, null=True)
-    endereco = models.ForeignKey(Endereco, related_name="pessoas", null=True, blank=False)
+    endereco = models.ForeignKey(Endereco, related_name="pessoas", null=True, blank=False, on_delete=models.CASCADE)
     #usuario = models.OneToOneField(User)
 
     def __str__(self):
@@ -36,14 +36,14 @@ class Evento(models.Model):
     ano = models.IntegerField(max_length=4)
     data_inicio = models.DateField(blank=True, null=True)
     data_fim = models.DateField(blank=True, null=True)
-    endereco = models.ForeignKey(Endereco, related_name="eventos", null=True, blank=False)
-    realizador = models.ForeignKey(PessoaFisica, null=True, blank=False)
+    endereco = models.ForeignKey(Endereco, related_name="eventos", null=True, blank=False, on_delete=models.CASCADE)
+    realizador = models.ForeignKey(PessoaFisica, null=True, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
 
 class Inscricao(models.Model):
-    participante = models.ForeignKey(PessoaFisica, related_name="inscrições", null=True, blank=False)
-    evento = models.ForeignKey(Evento, related_name="inscrições", null=True, blank=False)
+    participante = models.ForeignKey(PessoaFisica, related_name="inscrições", null=True, blank=False, on_delete=models.CASCADE)
+    evento = models.ForeignKey(Evento, related_name="inscrições", null=True, blank=False, on_delete=models.CASCADE)
     data_inscricao = models.DateTimeField(blank=True, null=True)
     preco = models.FloatField()
